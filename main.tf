fb12f70c830f5477149e47ab264871360b5fe582
greyhoundforty <ryantiffany@fastmail.com>
2020-08-18 13:53:54 -0500

    Testing with updated main.tf
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "ibm_compute_vm_instance" "node" {
   hourly_billing       = true
   private_network_only = false
   local_disk           = true
-  user_metadata        = file("${path.module}/install.yml)
+  user_metadata        = file("${path.module}/install.yml")
   flavor_key_name      = var.flavor
   tags                 = [var.datacenter]
   ssh_key_ids          = [data.ibm_compute_ssh_key.deploymentKey.id]
